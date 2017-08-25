df8bbe45f3ff63f626e99b2e0f082289c1150003
btaba <trex@Mac-Pro.local>
2017-08-24 23:35:43 -0400

    git config to provision

--- provision.sh
+++ provision.sh
@@ -1,6 +1,11 @@
 # Brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Git
+brew install git
+git config --global user.name "btaba"
+git config --global credential.helper osxkeychain
+
 # Bash Autocomplete
 brew install bash-completion
 brew tap homebrew/completions
