1c10d3e12a44f5ceb66cb12a4efda179ba79d319
btaba <trex@Mac-Pro.local>
2017-08-24 23:31:13 -0400

    adding provision file and terminal settings

---
+++ provision.sh
@@ -0,0 +1,11 @@
+# Brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Bash Autocomplete
+brew install bash-completion
+brew tap homebrew/completions
+
+# Conda
+curl -O https://repo.continuum.io/archive/Anaconda3-4.4.0-MacOSX-x86_64.sh
+bash Anaconda3-4.4.0-MacOSX-x86_64.sh
+
